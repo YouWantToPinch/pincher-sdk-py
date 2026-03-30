bdcc3327cea050506c63e257467ed7a8a5cbe33c
bntrtm <git.bntrtm@tjbainter.com>
2026-03-30 15:17:27 -0500

    fix(client): use base_url in state request

--- src/pincher_sdk/client/_requests_state.py
+++ src/pincher_sdk/client/_requests_state.py
@@ -9,7 +9,7 @@ from pincher_sdk.endpoints import endpoint_server_readiness
 async def get_server_ready(self: "Client") -> bool:
     endpoint = endpoint_server_readiness()
     try:
-        response = await self.get(endpoint)
+        response = await self._do_request("GET", endpoint, None, handle_response=False)
         return 200 <= response.status_code < 300
     except Exception as e:
         raise e
