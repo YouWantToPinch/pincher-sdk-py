c467ba72eddb12116ad978a1c030512d439c8523
bntrtm <git.bntrtm@tjbainter.com>
2026-03-29 21:56:49 -0500

    feat(client): port payee request functions

---
+++ src/client/_requests_payees.py
@@ -0,0 +1,61 @@
+from client.types import Payee, BudgetResourceKind
+from cache import ResourceCacheEntry
+from client import Client
+from endpoints import endpoint_budget_payees, endpoint_budget_payee
+from client.payloads import (
+    BudgetPayeeCreateData,
+    BudgetPayeeUpdateData,
+    BudgetPayeeDeleteData,
+)
+
+
+async def budget_payee_create(self: Client, b_id: str, data: BudgetPayeeCreateData):
+    endpoint = endpoint_budget_payees(b_id)
+    try:
+        payee = await self._do_request("POST", endpoint, data, self._token)
+    except Exception as e:
+        raise e
+    self.cache.set(ResourceCacheEntry(payee, endpoint), b_id)
+
+
+async def budget_payee(self: Client, b_id: str, c_id: str) -> Payee:
+    endpoint = endpoint_budget_payee(b_id, c_id)
+    try:
+        payee = await self._do_request("GET", endpoint, None, self._token)
+    except Exception as e:
+        raise e
+    return payee
+
+
+async def budget_payees(self: Client, b_id: str, c_id: str) -> list[Payee]:
+    endpoint = endpoint_budget_payees(b_id)
+    try:
+        payees = await self._do_request("GET", endpoint, None, self._token)
+    except Exception as e:
+        raise e
+    return payees
+
+
+async def budget_payee_update(
+    self: Client, b_id: str, c_id: str, data: BudgetPayeeUpdateData
+):
+    endpoint = endpoint_budget_payee(b_id, c_id)
+    try:
+        await self._do_request("PUT", endpoint, data, self._token)
+        self.budget_payee(b_id, c_id)  # type: ignore
+    except Exception as e:
+        raise e
+
+
+async def budget_payee_delete(
+    self: Client,
+    b_id: str,
+    c_id: str,
+    data: BudgetPayeeDeleteData,
+):
+    endpoint = endpoint_budget_payee(b_id, c_id)
+    try:
+        await self._do_request("DELETE", endpoint, data, self._token)
+        self.cache.delete(b_id, c_id, BudgetResourceKind.PAYEE)
+    except Exception as e:
+        raise e
