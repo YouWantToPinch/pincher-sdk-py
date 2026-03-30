b9c9df9e2c2850e58ca646815b724b86c8627055
bntrtm <git.bntrtm@tjbainter.com>
2026-03-29 22:12:02 -0500

    feat(client): port txn request functions

---
+++ src/client/_requests_txns.py
@@ -0,0 +1,88 @@
+from client.types import Transaction, BudgetResourceKind
+from cache import ResourceCacheEntry
+from client import Client
+from endpoints import (
+    endpoint_budget_transactions,
+    endpoint_budget_transaction,
+    endpoint_budget_transaction_details,
+    endpoint_budget_transactions_details,
+)
+from client.payloads import (
+    BudgetTransactionCreateData,
+    BudgetTransactionUpdateData,
+)
+
+
+async def budget_transaction_create(
+    self: Client, b_id: str, data: BudgetTransactionCreateData
+):
+    endpoint = endpoint_budget_transactions(b_id)
+    try:
+        transaction = await self._do_request("POST", endpoint, data, self._token)
+    except Exception as e:
+        raise e
+    self.cache.set(ResourceCacheEntry(transaction, endpoint), b_id)
+
+
+async def budget_transaction(self: Client, b_id: str, t_id: str) -> Transaction:
+    endpoint = endpoint_budget_transaction(b_id, t_id)
+    try:
+        transaction = await self._do_request("GET", endpoint, None, self._token)
+    except Exception as e:
+        raise e
+    return transaction
+
+
+async def budget_transactions(self: Client, b_id: str, t_id: str) -> list[Transaction]:
+    endpoint = endpoint_budget_transactions(b_id)
+    try:
+        transactions = await self._do_request("GET", endpoint, None, self._token)
+    except Exception as e:
+        raise e
+    return transactions
+
+
+async def budget_transaction_details(self: Client, b_id: str, t_id: str) -> Transaction:
+    endpoint = endpoint_budget_transaction_details(b_id, t_id)
+    try:
+        transaction = await self._do_request("GET", endpoint, None, self._token)
+    except Exception as e:
+        raise e
+    return transaction
+
+
+async def budget_transactions_details(
+    self: Client, b_id: str, t_id: str
+) -> list[Transaction]:
+    endpoint = endpoint_budget_transactions_details(b_id)
+    try:
+        transactions = await self._do_request("GET", endpoint, None, self._token)
+    except Exception as e:
+        raise e
+    return transactions
+
+
+async def budget_transaction_update(
+    self: Client, b_id: str, t_id: str, data: BudgetTransactionUpdateData
+):
+    endpoint = endpoint_budget_transaction(b_id, t_id)
+    try:
+        await self._do_request("PUT", endpoint, data, self._token)
+        self.budget_transaction(b_id, t_id)  # type: ignore
+        self.budget_transaction_details(b_id, t_id)  # type: ignore
+    except Exception as e:
+        raise e
+
+
+async def budget_transaction_delete(
+    self: Client,
+    b_id: str,
+    t_id: str,
+):
+    endpoint = endpoint_budget_transaction(b_id, t_id)
+    try:
+        await self._do_request("DELETE", endpoint, None, self._token)
+        self.cache.delete(b_id, t_id, BudgetResourceKind.TRANSACTION)
+        self.cache.delete(b_id, t_id, BudgetResourceKind.TRANSACTION_DETAIL)
+    except Exception as e:
+        raise e
